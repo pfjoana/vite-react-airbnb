15025f0a315a2d982b9653cc3b823bdb9da59b7e
joana faria <paisdefaria.joana@gmail.com>
2024-01-05 12:19:49 +0000

    add hero component, its img and text

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+
 import './App.css'
 
 export default function App() {
   return (
       <div className="container">
           <Navbar/>
+          <Hero/>
       </div>
   )
 }
