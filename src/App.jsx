655a1a1d9c8e13407371d600e4ffc6d8c63411e2
joana faria <paisdefaria.joana@gmail.com>
2024-01-13 14:39:34 +0000

    delete empty lines

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,21 @@ import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Card from './components/Card.jsx'
 import data from './data.jsx'
-
-
-
 import './App.css'
 
 export default function App() {
   const cards = data.map(item => {
     return (
-        <Card
-              img = {item.coverImg}
-              rating = {item.stats.rating}
-              reviewCount = {item.stats.reviewCount}
-              location = {item.location}
-              title = {item.title}
-              price = {item.price}
-              />
-  )
+      <Card
+        key = {item.id}
+        img = {item.coverImg}
+        rating = {item.stats.rating}
+        reviewCount = {item.stats.reviewCount}
+        location = {item.location}
+        title = {item.title}
+        price = {item.price}
+      />
+    )
   })
 
   return (
