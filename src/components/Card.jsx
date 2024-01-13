a6c49aba18c4320f68a64638a2709088327f22b5
joana faria <paisdefaria.joana@gmail.com>
2024-01-13 17:08:06 +0000

    change props using spread

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,24 @@
 import star from "../../public/images/star.png"
 
 export default function Card(props) {
+  console.log(props)
   let badgeText
   if (props.openSpots === 0) {
       badgeText = "SOLD OUT"
   } else if (props.location === "Online") {
       badgeText = "ONLINE"
   }
+
   return (
     <div className="card">
       {badgeText && <div className="card-badge">{badgeText}</div>}
 
-      <img src={`../public/images/${props.img}`} className="card-img"/>
+      <img src={`../public/images/${props.coverImg}`} className="card-img"/>
 
       <div className="card-stats">
         <img src={star} className="card-star"/>
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
+        <span>{props.stats.rating}</span>
+        <span className="gray">({props.stats.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
       </div>
 
